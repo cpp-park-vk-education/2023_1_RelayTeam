204c35929273f67a4a391048b47ab1325cf6a085
AlfaIV <AlfaIV@yandex.ru>
2023-05-03 11:34:39 +0300

    add simple port scaner

--- application/sources/main.cpp
+++ application/sources/main.cpp
@@ -2,15 +2,21 @@
 #include <QtDebug>
 
 #include "MainWindow.h"
+#include "SessionManager.h"
 #include "variables.h"
 
 int main(int argc, char* argv[]) {
-	qDebug() << "Application started.";
-	QApplication a(argc, argv);
-	a.setApplicationDisplayName("MultiRelay");
-	a.setApplicationName("MultiRelay");
-	MainWindow w;
-	w.show();
+    qDebug() << "Application started.";
+    //	QApplication a(argc, argv);
+    //	a.setApplicationDisplayName("MultiRelay");
+    //	a.setApplicationName("MultiRelay");
+    //	MainWindow w;
+    //	w.show();
 
-	return a.exec();
+    //	return a.exec();
+    SessionManager Ses;
+    //    127.0.0.54:53
+    qDebug() << Ses.portIsBusy("127.0.0.54", 53);
+    qDebug() << Ses.portIsBusy("localhost", 631);
+    qDebug() << Ses.getPort();
 }
--- gstreamer_interface/include/SessionManager.h
+++ gstreamer_interface/include/SessionManager.h
@@ -8,38 +8,42 @@
 
 #include <QMap>
 #include <QObject>
+#include <QTcpSocket>
 #include <QtNetwork/QHostAddress>
 #include <cstring>
 
 class SessionManager : public QObject {
 private:
     Q_OBJECT
-	QHash<QPair<QHostAddress, QString>, std::shared_ptr<Session>> live_sessions;  // map<session_id, session>
+    QHash<QPair<QHostAddress, QString>, std::shared_ptr<Session>> live_sessions;  // map<session_id, session>
     // void handleException(GstreamerError error);	 // Provides flowless application work after gstreamer errors.
 
-	void startThread(Session* session);
+    void startThread(Session* session);
 
 public:
-	SessionManager();
+    bool portIsBusy(QString ip, qint16 port);
+    qint16 getPort();
 
-	~SessionManager();
+    SessionManager();
+
+    ~SessionManager();
 
 public slots:
-	void onStartVideoSession(const QHostAddress ip_address);
+    void onStartVideoSession(const QHostAddress ip_address);
 
-	void onStartAudioSession(const QHostAddress ip_address);
+    void onStartAudioSession(const QHostAddress ip_address);
 
-	void onKillVideoSession(const QHostAddress ip_address);
+    void onKillVideoSession(const QHostAddress ip_address);
 
-	void onKillAudioSession(const QHostAddress ip_address);
+    void onKillAudioSession(const QHostAddress ip_address);
 
-	void onStartReceivingSession(const QMdnsEngine::Message message_received, const QString session_type);
+    void onStartReceivingSession(const QMdnsEngine::Message message_received, const QString session_type);
 
-	void onKillVideoReciver(const QHostAddress ip_address);
+    void onKillVideoReciver(const QHostAddress ip_address);
 
-	void onKillAudioReciver(const QHostAddress ip_address);
+    void onKillAudioReciver(const QHostAddress ip_address);
 
-	void onReceivedPorts(const QHostAddress ip_address, qint16 video_port, qint16 audio_port);
+    void onReceivedPorts(const QHostAddress ip_address, qint16 video_port, qint16 audio_port);
 
 signals:
 	void sendErrorOccured(const QString error_string);
