558f73b6608dde4517a64d68544cb2cbd602f9e2
AlfaIV <AlfaIV@yandex.ru>
2023-05-04 16:18:51 +0300

    delete default arguments in func get port

--- general_use/include/networkTools.h
+++ general_use/include/networkTools.h
@@ -20,6 +20,6 @@ QHostAddress getIPv6();
 
 QString processIPv6(const QHostAddress& ipv6_address);
 
-qint32 getPort(qint32 startRangeSearch = 4000, qint32 stopRangeSearch = 5000, QHostAddress ip_host_address = QHostAddress::LocalHost);
+qint32 getPort(qint32 startRangeSearch, qint32 stopRangeSearch, QHostAddress ip_host_address);
 
 bool portIsBusy(const QHostAddress& ipv4_address, qint32 port);
