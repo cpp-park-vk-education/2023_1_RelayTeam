43d6c93fa1824601f1ced1884cf6a0ad62c099ce
Ant0nL0rd <gerasin_03@mail.ru>
2023-05-23 22:21:36 +0300

    Unneeded files deleted.

--- general_use/include/variables.h
+++ general_use/include/variables.h
@@ -1,8 +1,8 @@
 #pragma once
 #include <QDir>
 
-#define PROJECT_PATH "/home/andr/project/2023_1_RelayTeam-QStreamer_feature"
+#define PROJECT_PATH "/home/anton/Documents/GitHubSide/2023_1_RelayTeam"
 
-#define RESOURCES_PATH "/home/andr/project/2023_1_RelayTeam-QStreamer_feature/resources"
+#define RESOURCES_PATH "/home/anton/Documents/GitHubSide/2023_1_RelayTeam/resources"
 
 const QDir Q_RESOURCE_DIR(RESOURCES_PATH);
